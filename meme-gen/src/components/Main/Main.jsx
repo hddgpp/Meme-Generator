5c1c4760bbe03f4fe622a6c2a0b94f47c6073f17
hddgpp <youssefhehe3@gmail.com>
2025-10-07 19:57:57 +0100

    making the inputs intractive

--- meme-gen/src/components/Main/Main.jsx
+++ meme-gen/src/components/Main/Main.jsx
@@ -2,17 +2,38 @@ import React from 'react';
 import './Main.css';
 
 export default function Main() {
+    const [meme, SetMeme] = React.useState({
+        memeImg: 'https://i.imgflip.com/1bij.jpg',
+        topRow: 'TOP TEXT',
+        bottomRow: 'BOTTOM TEXT'
+    })
+    function changeTop(event){
+        const {value} = event.currentTarget 
+        SetMeme(prev => ({...prev, topRow: value.length === 0 ? 'TOP TEXT': value}))
+    }
+    function changeBottom(event){
+        const {value} = event.currentTarget 
+        SetMeme(prev => ({...prev, bottomRow: value.length === 0 ? 'BOTTOM TEXT' : value  }))
+    }
   return (
     <main className="main-container">
       <div className="input-box">
         <div className="input-fields">
           <label>
             Upper Text
-            <input type="text" placeholder='shut up' name='Upper-text'/>
+            <input 
+             type="text"
+             placeholder='shut up'
+             name='Upper-text'
+             onChange={changeTop}/>
           </label>
           <label>
             Bottom Text
-            <input type="text" placeholder='and take my money' name='Bottom-text'/>
+            <input 
+             type="text" 
+             placeholder='and take my money' 
+             name='Bottom-text'
+             onChange={changeBottom}/>
           </label>
         </div>
         <div className="button-container">
@@ -20,9 +41,9 @@ export default function Main() {
         </div>
       </div>
       <div className='meme-box'>
-        <img src="https://i.imgflip.com/1bij.jpg" alt="Meme template" />
-        <span className='Top'>TEST TOP TEXT</span>
-        <span className='bottom'>TEST BOTTOM TEXT</span>
+        <img src={meme.memeImg} alt="Meme template" />
+        <span className='Top'>{meme.topRow}</span>
+        <span className='bottom'>{meme.bottomRow}</span>
       </div>
       <button className='download'>Download</button>
     </main>

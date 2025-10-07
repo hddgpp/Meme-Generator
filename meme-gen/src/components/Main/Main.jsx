d55237b1d31ddec18b562ec95884dabaa9eb6c6a
hddgpp <youssefhehe3@gmail.com>
2025-10-07 19:00:29 +0100

    setting up the Main layout

--- meme-gen/src/components/Main/Main.jsx
+++ meme-gen/src/components/Main/Main.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import './Main.css'
+import React from 'react';
+import './Main.css';
 
 export default function Main() {
-
-    return(
-        <main>
-            
-        </main>
-    )
+  return (
+    <main className="main-container">
+      <div className="input-box">
+        <div className="input-fields">
+          <label>
+            Upper Text
+            <input type="text" placeholder='shut up' name='Upper-text'/>
+          </label>
+          <label>
+            Bottom Text
+            <input type="text" placeholder='and take my money' name='Bottom-text'/>
+          </label>
+        </div>
+        <div className="button-container">
+          <button className='get-img'>Get a new img</button>
+        </div>
+      </div>
+      <div className='meme-box'>
+        <img src="https://i.imgflip.com/1bij.jpg" alt="Meme template" />
+        <span className='Top'>TEST TOP TEXT</span>
+        <span className='bottom'>TEST BOTTOM TEXT</span>
+      </div>
+      <button className='download'>Download</button>
+    </main>
+  );
 }
